2b26668436a2bc22145844f7ecf3b8f24ac1069a
TeckGuy <gridtechideal@gmail.com>
2018-01-05 10:06:12 +0300

    v3.0 bug fix

--- v 3.0/database/dbpharmacy.sql
+++ v 3.0/database/dbpharmacy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 01, 2018 at 04:03 PM
+-- Generation Time: Jan 05, 2018 at 08:04 AM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -44,7 +44,8 @@ CREATE TABLE `adminlogin` (
 --
 
 INSERT INTO `adminlogin` (`NationalID`, `username`, `personalnumber`, `password`, `Salary`, `Payed`, `Balance`, `LastEdited`) VALUES
-(31407936, 'TechGuy', '6668', '93f725a07423fe1c889f448b33d21f46', 10000, 1000, 9000, 'Monday, 01 January 2018, 05:07:09.141 PM');
+(31407936, 'TechGuy', '6668', '93f725a07423fe1c889f448b33d21f46', 10000, 1000, 9000, 'Monday, 01 January 2018, 05:07:09.141 PM'),
+(87654321, 'mercy', '777', NULL, 7000, NULL, 7000, 'Friday, 05 January 2018, 09:45:53.525 AM');
 
 -- --------------------------------------------------------
 
@@ -65,6 +66,14 @@ CREATE TABLE `pastrecords` (
   `Loss` double NOT NULL,
   `LastEdited` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pastrecords`
+--
+
+INSERT INTO `pastrecords` (`Mname`, `Mserial`, `StockIn`, `TaxIn`, `StockInCost`, `StockOut`, `TaxOut`, `StockOutCost`, `Profit`, `Loss`, `LastEdited`) VALUES
+('Lamboghini', 'Bull', 10, 32, 232, 10, 40, 290, 58, 0, 'Wednesday, 03 January 2018, 10:03:58.594 PM'),
+('vv', 'fff', 5, 0, 20, 0, 0, 0, 0, 20, 'Thursday, 04 January 2018, 09:30:18.665 PM');
 
 -- --------------------------------------------------------
 
@@ -88,7 +97,9 @@ CREATE TABLE `paymentrecords` (
 
 INSERT INTO `paymentrecords` (`Cname`, `CIdentity`, `Tcost`, `Payed`, `Balance`, `Dpayed`, `LastEdited`) VALUES
 ('TechUser', '12345678', 5000, 1000, 4000, '2018-01-01', 'Monday, 01 January 2018, 05:07:09.141 PM'),
-('TechGuy', '31407936', 10000, 1000, 9000, '2018-01-01', 'Monday, 01 January 2018, 05:07:09.141 PM');
+('TechGuy', '31407936', 10000, 1000, 9000, '2018-01-01', 'Monday, 01 January 2018, 05:07:09.141 PM'),
+('TechUser', '12345678', 5000, 1090, 3910, '2018-01-03', 'Wednesday, 03 January 2018, 09:58:19.925 PM'),
+('Italy', 'Lamboghini', 232, 12, 220, '2018-01-03', 'Wednesday, 03 January 2018, 10:07:34.395 PM');
 
 -- --------------------------------------------------------
 
@@ -119,9 +130,10 @@ CREATE TABLE `store` (
 --
 
 INSERT INTO `store` (`Mname`, `Mserial`, `Mquantity`, `Mcost`, `TotalTax`, `Mtotalcost`, `Medate`, `Msection`, `Mprescription`, `Mdistributer`, `Mcategory`, `Mreport`, `TaxType`, `UpdatedOn`, `LastEdited`) VALUES
-('Lamboghini', 'Bull', 7, 20, 22.4, 162.4, '2018-01-31', 'Dispensary', '1x4', 'Italy', 'Veneno', 'Racing ega Ccar', 'Taxable', 'Monday, 01 January 2018, 05:09:47.284 PM', 'Monday, 01 January 2018, 05:09:47.284 PM'),
-('Nissan', 'GTR', 100, 100, 1600, 11600, '2018-01-31', 'Dispensary', '1X4', 'Japan', 'nismo', 'Racing Car', 'Taxable', 'Monday, 01 January 2018, 05:07:36.546 PM', 'Monday, 01 January 2018, 05:07:36.546 PM'),
-('Ferrari', 'LaFerrari', 100, 200, 3200, 23200, '2018-01-31', 'Display', '1x3', 'U.S.A', 'Red One', 'Racing Super Car', 'Taxable', 'Monday, 01 January 2018, 05:16:00.498 PM', 'Monday, 01 January 2018, 05:16:00.498 PM');
+('Nissan', 'GTR', 0, 100, -1328, -9628, '2018-01-31', 'Dispensary', '1X4', 'Japan', 'nismo', 'Racing Car', 'Taxable', 'Monday, 01 January 2018, 05:07:36.546 PM', 'Monday, 01 January 2018, 05:07:36.546 PM'),
+('hh', 'hh', 7, 6, 6.72, 48.72, '2018-01-05', 'Dispensary', '787', 'nn', 'nn', 'nnn', 'Taxable', 'Friday, 05 January 2018, 09:44:08.588 AM', 'Friday, 05 January 2018, 09:44:08.588 AM'),
+('Ferrari', 'LaFerrari', 65, 200, 704, 5104, '2018-01-31', 'Display', '1x3', 'U.S.A', 'Red One', 'Racing Super Car', 'Taxable', 'Monday, 01 January 2018, 05:16:00.498 PM', 'Monday, 01 January 2018, 05:16:00.498 PM'),
+('66', 'uiu', 94, 4, 0, 376, '2018-01-11', 'Display', 'uiyt', 'hh', 'yy', 'nn', 'Non-taxable', 'Friday, 05 January 2018, 09:44:08.588 AM', 'Friday, 05 January 2018, 09:44:08.588 AM');
 
 -- --------------------------------------------------------
 
@@ -149,9 +161,10 @@ CREATE TABLE `tablecritical` (
 --
 
 INSERT INTO `tablecritical` (`Mname`, `Mserial`, `StockIn`, `TaxIn`, `StockInCost`, `StockOut`, `TaxOut`, `StockOutCost`, `Profit`, `Loss`, `UpdatedOn`, `LastEdited`) VALUES
-('Lamboghini', 'Bull', 10, 32, 232, 0, 0, 0, 0, 232, 'Monday, 01 January 2018, 05:17:27.060 PM', 'Monday, 01 January 2018, 05:17:27.060 PM'),
-('Nissan', 'GTR', 100, 1600, 11600, 0, 0, 0, 0, 11600, 'Monday, 01 January 2018, 05:17:27.060 PM', 'Monday, 01 January 2018, 05:17:27.060 PM'),
-('Ferrari', 'LaFerrari', 100, 3200, 23200, 0, 0, 0, 0, 23200, 'Monday, 01 January 2018, 05:17:27.060 PM', 'Monday, 01 January 2018, 05:17:27.060 PM');
+('Nissan', 'GTR', 100, 1600, 11600, 100, 2928, 21228, 9628, 0, 'Friday, 05 January 2018, 09:47:24.667 AM', 'Monday, 01 January 2018, 05:17:27.060 PM'),
+('hh', 'hh', 7, 6.72, 48.72, 0, 0, 0, 0, 48.72, 'Friday, 05 January 2018, 09:47:24.667 AM', 'Friday, 05 January 2018, 09:47:24.667 AM'),
+('Ferrari', 'LaFerrari', 100, 3200, 23200, 27, 864, 6264, 0, 16936, 'Friday, 05 January 2018, 09:47:24.667 AM', 'Monday, 01 January 2018, 05:17:27.060 PM'),
+('66', 'uiu', 99, 0, 396, 5, 0, 20, 0, 376, 'Friday, 05 January 2018, 09:47:24.667 AM', 'Friday, 05 January 2018, 09:47:24.667 AM');
 
 -- --------------------------------------------------------
 
@@ -175,9 +188,12 @@ CREATE TABLE `tabledistributers` (
 --
 
 INSERT INTO `tabledistributers` (`Mserial`, `Dname`, `Mname`, `Mquantity`, `Mtotalcost`, `Payed`, `Balance`, `LastEdited`) VALUES
-('Bull', 'Italy', 'Lamboghini', 10, 232, NULL, 232, 'Monday, 01 January 2018, 05:09:47.284 PM'),
+('Bull', 'Italy', 'Lamboghini', 10, 232, 12, 220, 'Wednesday, 03 January 2018, 10:07:34.395 PM'),
+('fff', 'dd', 'vv', 5, 20, NULL, 20, 'Thursday, 04 January 2018, 09:28:32.723 PM'),
 ('GTR', 'Japan', 'Nissan', 100, 11600, NULL, 11600, 'Monday, 01 January 2018, 05:07:36.546 PM'),
-('LaFerrari', 'U.S.A', 'Ferrari', 100, 23200, NULL, 23200, 'Monday, 01 January 2018, 05:16:00.498 PM');
+('hh', 'nn', 'hh', 7, 48.72, NULL, 48.72, 'Friday, 05 January 2018, 09:44:08.588 AM'),
+('LaFerrari', 'U.S.A', 'Ferrari', 100, 23200, NULL, 23200, 'Monday, 01 January 2018, 05:16:00.498 PM'),
+('uiu', 'hh', '66', 99, 396, NULL, 396, 'Friday, 05 January 2018, 09:44:08.588 AM');
 
 -- --------------------------------------------------------
 
@@ -206,7 +222,20 @@ CREATE TABLE `tablesell` (
 --
 
 INSERT INTO `tablesell` (`Mname`, `Mserial`, `Mtotalquantity`, `Bcost`, `Mcost`, `TotalTax`, `Tamount`, `Mtotalcost`, `CashPaid`, `ChangePaid`, `Msolddate`, `LastEdited`, `Invoice`) VALUES
-('Lamboghini', 'Bull', 3, 20, 20, 9.600000000000001, 60, 69.6, 100, 40, '2018-01-01', 'Monday, 01 January 2018, 05:18:50.924 PM', '-1191859900');
+('Nissan', 'GTR', 4, 100, 100, 64, 400, 464, 400, 0, '2018-01-01', 'Monday, 01 January 2018, 06:46:41.619 PM', '-1498992335'),
+('Ferrari', 'LaFerrari', 5, 200, 200, 160, 1000, 1160, 1000, 0, '2018-01-01', 'Monday, 01 January 2018, 07:00:18.692 PM', '-694111214'),
+('Nissan', 'GTR', 3, 100, 100, 48, 300, 348, 500, 200, '2018-01-03', 'Wednesday, 03 January 2018, 09:15:24.782 AM', '-735233232'),
+('Nissan', 'GTR', 5, 100, 100, 80, 500, 580, 700, 200, '2018-01-04', 'Thursday, 04 January 2018, 08:44:10.808 AM', '2088150140'),
+('Ferrari', 'LaFerrari', 5, 200, 200, 160, 1000, 1160, 1000, 0, '2018-01-04', 'Thursday, 04 January 2018, 08:49:29.807 AM', '761457484'),
+('Nissan', 'GTR', 2, 100, 100, 32, 200, 232, 200, 0, '2018-01-04', 'Thursday, 04 January 2018, 09:52:50.560 AM', '-771878605'),
+('Ferrari', 'GTR', 4, 200, 200, 128, 800, 928, 1000, 200, '2018-01-04', 'Thursday, 04 January 2018, 09:54:38.157 AM', '-1197019868'),
+('Nissan', 'GTR', 2, 100, 100, 32, 200, 232, 400, 200, '2018-01-04', 'Thursday, 04 January 2018, 10:08:26.585 AM', '-1317631827'),
+('Ferrari', 'LaFerrari', 4, 200, 200, 128, 800, 928, 1000, 200, '2018-01-04', 'Thursday, 04 January 2018, 10:20:24.343 AM', '2001861050'),
+('Nissan', 'GTR', 1, 100, 100, 16, 100, 116, 100, 0, '2018-01-04', 'Thursday, 04 January 2018, 10:22:44.213 AM', '-1802376842'),
+('Nissan', 'GTR', 79, 100, 200, 2528, 15800, 18328, 16000, 200, '2018-01-04', 'Thursday, 04 January 2018, 03:20:37.183 PM', '220920935'),
+('Ferrari', 'LaFerrari', 8, 200, 200, 256, 1600, 1856, 2000, 400, '2018-01-05', 'Friday, 05 January 2018, 08:28:20.739 AM', '-1548844140'),
+('Ferrari', 'LaFerrari', 5, 200, 200, 160, 1000, 1160, 1000, 0, '2018-01-05', 'Friday, 05 January 2018, 09:41:13.357 AM', '-287982819'),
+('66', 'uiu', 5, 4, 4, 0, 20, 20, 50, 30, '2018-01-05', 'Friday, 05 January 2018, 09:46:43.066 AM', '1894247581');
 
 -- --------------------------------------------------------
 
@@ -227,9 +256,10 @@ CREATE TABLE `tablestockin` (
 --
 
 INSERT INTO `tablestockin` (`Mname`, `Mserial`, `Mtotalquantity`, `TotalTax`, `Mtotalcost`) VALUES
-('Lamboghini', 'Bull', 10, 32, 232),
 ('Nissan', 'GTR', 100, 1600, 11600),
-('Ferrari', 'LaFerrari', 100, 3200, 23200);
+('hh', 'hh', 7, 6.72, 48.72),
+('Ferrari', 'LaFerrari', 100, 3200, 23200),
+('66', 'uiu', 99, 0, 396);
 
 -- --------------------------------------------------------
 
@@ -253,7 +283,7 @@ CREATE TABLE `userlogin` (
 --
 
 INSERT INTO `userlogin` (`NationalID`, `username`, `personalnumber`, `password`, `Salary`, `Payed`, `Balance`, `LastEdited`) VALUES
-(12345678, 'TechUser', '6668', '827ccb0eea8a706c4c34a16891f84e7b', 5000, 1000, 4000, 'Monday, 01 January 2018, 05:07:09.141 PM');
+(12345678, 'TechUser', '6668', '93f725a07423fe1c889f448b33d21f46', 5000, 1090, 3910, 'Wednesday, 03 January 2018, 09:58:19.925 PM');
 
 --
 -- Indexes for dumped tables
